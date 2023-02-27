8553e58766c3d81173fd1de431d0157bc5f4e59b
lamyaa1337 <lamyaa.berri@gmail.com>
2023-02-27 13:22:18 -0800

    l

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
-*_puts - print a string to stdout
-* @str: char array string type
-* Description: Can only use _putchar
-*/
+ * _puts - print a string to stdout
+ * @str: char array string type
+ * Description: Can only use _putchar
+ */
 
 void _puts(char *str)
 {
-int i;
+	int i;
 
-for (i = 0; str[i] != '\0'; i++)
-{
-	_putchar(str[i]);
-}
-_putchar('\n');
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
