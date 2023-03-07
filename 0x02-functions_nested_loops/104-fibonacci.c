06bab9c96dcf65a4fdf2dbdc7d49f4a67fd6554d
lamyaa1337 <lamyaa.berri@gmail.com>
2023-03-07 13:59:33 -0800

    y

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,32 +1,50 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - finds and prints the sum of the even-valued terms
- * followed by a new line
- * Return: Always 0 (Success)
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
  */
 
-int main(void)
+void print_times_table(int n)
 {
-	int i;
-	unsigned long int j, k, next, sum;
+	int x = 0, y, z;
 
-	j = 1;
-	k = 2;
-	sum = 0;
-
-	for (i = 1; i <= 33; ++i)
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
 	{
-		if (j < 4000000 && (j % 2) == 0)
+		for (y = 0; y <= n; y++)
 		{
-			sum = sum + j;
+			z = x * y;
+			if (z > 99)
+			{
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-		next = j + k;
-		j = k;
-		k = next;
+		_putchar('\n');
+		x++;
 	}
-
-	printf("%lu\n", sum);
-
-	return (0);
 }
